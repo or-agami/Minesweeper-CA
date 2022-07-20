7a51fa68c6a8297f9198f9d5065e272ad18c5876
or-agami <oragami0@gmail.com>
2022-07-20 16:03:37 +0300

    init js and game logic

---
+++ js/utils.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function printMat(mat, selector) {
+
+    var strHTML = ''
+    for (var i = 0; i < mat.length; i++) {
+
+        strHTML += '<tr>'
+        for (var j = 0; j < mat[0].length; j++) {
+
+            const cell = mat[i][j]
+            const className = 'cell cell-' + i + '-' + j
+            strHTML += `<td class="${className}">${cell}</td>`
+        }
+        strHTML += '</tr>'
+    }
+    
+    const elContainer = document.querySelector(selector)
+    elContainer.innerHTML = strHTML
+}
