e3136d243d7ce7c37bc53c4b56f78ebb8c3faf5d
or-agami <oragami0@gmail.com>
2022-07-20 16:03:20 +0300

    init js and game logic

--- js/game.js
+++ js/game.js
@@ -1,5 +1,55 @@
+'use strict'
 
+const EMPTY = ''
+const MINE = '💣'
+const FLAG = '🚩'
 
-function initGame() {
-    console.log('hello');
+var gBoard;
+var gLevel = {
+	size: 4,
+	mines: 2,
+};
+var gGame = {
+	isOn: false,
+	shownCount: 0,
+	markedCount: 0,
+	flagsCount: 0,
+	interval: 0,
+	startTime: 0,
+	livesCount: 3,
+	spoilCount: 3,
+};
+
+function initGame(size = 4, mines = 2) {
+    const elDefaultLevel = document.querySelector('.difficulty-buttons button')
+    elDefaultLevel.style.backgroundColor = '#40798C'
+    levelSelect(size, mines, elDefaultLevel)
+}
+
+function levelSelect(size, mines, elButton) {
+    clearInterval(gGame.interval)
+    const elButtons = document.querySelectorAll('.difficulty-buttons button')
+    for (let i = 0; i < elButtons.length; i++) {
+        const elLevelButton = elButtons[i];
+        if (elButton === elLevelButton) {
+            elButton.style.backgroundColor = '#40798C'
+        }
+        else elLevelButton.removeAttribute('style')
+    }
+    gLevel.size = size
+    gLevel.mines = mines
+    const elBoard = document.querySelector('.board')
+    // printMat(createBoard(size), elBoard)
+    console.log('createBoard(size):', createBoard(size));
+}
+
+function createBoard(size, mines) {
+    gBoard = []
+    for (let i = 0; i < size; i++) {
+        gBoard
+        for (let j = 0; j < size; j++) {
+            gBoard[i][j] = `${j}`
+        }
+    }
+    return gBoard
 }
